39d294dcc7253918344e1ce70d4356a268aa3ad3
yldv <abdulazizy5955@icloud.com>
2025-06-26 14:21:57 +0500

    .

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class Foods(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='foods')
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    picture = models.URLField(default='https://www.google.com/imgres?q=food&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F6%2F6d%2FGood_Food_Display_-_NCI_Visuals_Online.jpg%2F1200px-Good_Food_Display_-_NCI_Visuals_Online.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFood&docid=F4fzcSFVcTK1kM&tbnid=nQBYUAp94s8s4M&vet=12ahUKEwjJkY3XpIiOAxU9U1UIHcC-CdMQM3oECBoQAA..i&w=1200&h=800&hcb=2&ved=2ahUKEwjJkY3XpIiOAxU9U1UIHcC-CdMQM3oECBoQAA')
+    picture = models.ImageField(upload_to='images/', null=True, blank=True)
     description = models.TextField()
     def __str__(self):
         return self.name
